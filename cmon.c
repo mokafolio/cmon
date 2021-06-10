d63af23a4bfe1a59912e136ecd529bb4b077141a
Matthias Dörfelt <hi@mokafolio.de>
2021-06-10 16:18:02 -0700

    added cmon_src_dir to create a cmon projects source and module hierarchy from a provided src directory

--- cmon.c
+++ cmon.c
@@ -1,2 +1,8 @@
 #include <cmon/cmon_builder_st.h>
 #include <cmon/cmon_fs.h>
+#include <cmon/cmon_argparse.h>
+
+int main(int _argc, const char * _args[])
+{
+    return EXIT_SUCCESS;
+}
---
+++ cmon/cmon_src_dir.h
@@ -0,0 +1,17 @@
+#ifndef CMON_CMON_SRC_DIR_H
+#define CMON_CMON_SRC_DIR_H
+
+#include <cmon/cmon_modules.h>
+#include <cmon/cmon_src.h>
+
+typedef struct cmon_src_dir cmon_src_dir;
+
+CMON_API cmon_src_dir * cmon_src_dir_create(cmon_allocator * _alloc,
+                                            const char * _path,
+                                            cmon_modules * _mods,
+                                            cmon_src * _src);
+CMON_API void cmon_src_dir_destroy(cmon_src_dir * _dir);
+CMON_API cmon_bool cmon_src_dir_parse(cmon_src_dir * _dir);
+CMON_API const char * cmon_src_dir_err_msg(cmon_src_dir * _dir);
+
+#endif // CMON_CMON_SRC_DIR_H
